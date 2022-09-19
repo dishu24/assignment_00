30245e5297466a6177ab2456963746179d2aed3b
dushyant <kapoordushyant6@gmail.com>
2022-09-20 05:08:11 +0530

    add mysql conf in django

--- api/api/settings.py
+++ api/api/settings.py
@@ -93,12 +93,27 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# SQLite
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# MySql database
+
+# DATABASES = {
+# 'default': {
+#     'ENGINE': 'django.db.backends.mysql',
+#     'NAME': 'E_Commerce',
+#     'USER': 'root',
+#     'PASSWORD': '',
+#     'HOST': 'localhost',
+#     'PORT': '3306'
+# }
+# }
 
 
 # Password validation
